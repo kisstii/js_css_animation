cad84e080d68ac89951d8d658809430469dd472a
Tamas Kiss <kisstii1@gmail.com>
2020-12-16 12:21:38 +0100

    ready

--- script.js
+++ script.js
@@ -1,8 +1,41 @@
 function pageLoad() {
 
+	const root = document.getElementById("root");
+
+	let myObjects = [
+		{
+			tag: "h1",
+			content: "Kiss Tamás"
+		},
+		{
+			tag: "p",
+			content: "Frontend developer"
+		}
+	];
+	
+	/*
+		for (let i = 0; i < MyObjects.length; i++) {
+					
+		}
+	*/
+
+	/*
+	for (myObject of myObjects) {
+		console.log(myObject);
+	};
+	*/
+
+	for (myObject of myObjects) {
+		root.insertAdjacentHTML("beforeend", `
+		<${myObject.tag}>${myObject.content}</${myObject.tag}>
+		`);
+	};
 
 
 
-};
+	root.addEventListener("click", function() {
+		root.classList.toggle("clicked");
+	});
 
+}
 window.addEventListener("load", pageLoad);
